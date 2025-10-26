d4b9aad7393ee575199fba5ac3339f5a5b62c2c6
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-10-26 20:29:16 +0100

    refactor: update database initialization to use neon client

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,4 +1,6 @@
 import { env } from '@/env';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 
-export const db = drizzle(env.DATABASE_URL);
+const sql = neon(env.DATABASE_URL);
+export const db = drizzle({ client: sql });
