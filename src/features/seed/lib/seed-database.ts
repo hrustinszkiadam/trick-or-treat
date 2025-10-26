7e5755290ebb186440b1dd7e72c3e358a2d0ca33
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-10-26 21:31:53 +0100

    fix: delete existing data before seeding

--- src/features/seed/lib/seed-database.ts
+++ src/features/seed/lib/seed-database.ts
@@ -1,9 +1,12 @@
 import { db } from '@/database';
 import { placeholderAddresses } from './placeholder-addresses';
 import { AddressTable } from '@/database/schema';
+import { Address } from '@/lib/definitions';
 
 export const seedDatabase = async (): Promise<object> => {
-  const results = await db
+  await db.delete(AddressTable);
+
+  const results: Address[] = await db
     .insert(AddressTable)
     .values(placeholderAddresses)
     .returning();
